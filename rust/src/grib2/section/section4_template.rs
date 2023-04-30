671f881b20f39fd892e24295358e58e897baa92e
seotaro <mail2017@seotaro.com>
2023-05-01 02:28:32 +0900

    add template 4.1, 4.11, 4.9, 4.50009, 4.50012

--- rust/src/grib2/section/section4_template.rs
+++ rust/src/grib2/section/section4_template.rs
@@ -5,16 +5,16 @@ use chrono::{DateTime, Utc};
 pub(crate) mod section4_template_impl;
 
 pub(crate) enum TemplateNumber<'a> {
-    T0(Template0<'a>), // 4.0
-    // 4.1
-    T8(Template8<'a>), // 4.8
-    // 4.9
-    // 4.11
+    T0(Template0<'a>),   // 4.0
+    T1(Template1<'a>),   // 4.1
+    T8(Template8<'a>),   // 4.8
+    T9(Template9<'a>),   // 4.9
+    T11(Template11<'a>), // 4.11
     // 4.50000
     T50008(Template50008<'a>), // 4.50008
-    // 4.50009
+    T50009(Template50009<'a>), // 4.50009
     T50011(Template50011<'a>), // 4.50011
-                               // 4.50012
+    T50012(Template50012<'a>), // 4.50012
                                // 4.50030
 }
 
@@ -35,9 +35,39 @@ pub(crate) struct Template0<'a> {
     pub(crate) buf: &'a [u8],
 }
 
+// template 4.1
+#[derive(Copy, Clone)]
+pub(crate) struct Template1<'a> {
+    pub(crate) buf: &'a [u8],
+}
+
 // template 4.8
 #[derive(Copy, Clone)]
 pub(crate) struct Template8<'a> {
+    pub(crate) buf: &'a [u8],
+}
+
+// template 4.9
+#[derive(Copy, Clone)]
+pub(crate) struct Template9<'a> {
+    pub(crate) buf: &'a [u8],
+}
+
+// template 4.11
+#[derive(Copy, Clone)]
+pub(crate) struct Template11<'a> {
+    pub(crate) buf: &'a [u8],
+}
+
+// template 4.50008
+#[derive(Copy, Clone)]
+pub(crate) struct Template50008<'a> {
+    pub(crate) buf: &'a [u8],
+}
+
+// template 4.50009
+#[derive(Copy, Clone)]
+pub(crate) struct Template50009<'a> {
     pub(crate) buf: &'a [u8],
 }
 
@@ -47,8 +77,8 @@ pub(crate) struct Template50011<'a> {
     pub(crate) buf: &'a [u8],
 }
 
-// template 4.50008
+// template 4.50012
 #[derive(Copy, Clone)]
-pub(crate) struct Template50008<'a> {
+pub(crate) struct Template50012<'a> {
     pub(crate) buf: &'a [u8],
 }
